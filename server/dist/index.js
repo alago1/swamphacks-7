0ff123bba5c83132c917081020f99b677c6b188c
Ranger Chenore <r2chenore@gmail.com>
2021-01-30 06:52:59 -0500

    Add distance calculation and make fetch_locations output closest locations in order

--- server/dist/index.js
+++ server/dist/index.js
@@ -27,8 +27,47 @@ const fetch_locations = (_, response) => {
         .get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location.lat},${location.lng}&radius=${radius}&type=${filter}&key=${process.env.googlemaps_api_key}`)
         .then((res) => {
         console.log(`${res.data.results.length} results`);
-        response.send(JSON.stringify(res.data.results, null, 2));
+        let closest_results = find_closest_results(location.lat, location.lng, res.data.results, 10);
+        response.send(JSON.stringify(closest_results, null, 2));
     });
 };
+function dist_between_coords(lat1, lng1, lat2, lng2) {
+    const R = 6371e3;
+    const φ1 = lat1 * Math.PI / 180;
+    const φ2 = lat2 * Math.PI / 180;
+    const Δφ = (lat2 - lat1) * Math.PI / 180;
+    const Δλ = (lng2 - lng1) * Math.PI / 180;
+    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c;
+    return d;
+}
+function find_closest_results(lat1, lng1, google_places_results, amt) {
+    let all_dists = [];
+    for (let i = 0; i < google_places_results.length; i++) {
+        const lat2 = google_places_results[i].geometry.location.lat;
+        const lng2 = google_places_results[i].geometry.location.lng;
+        const dist = dist_between_coords(lat1, lng1, lat2, lng2);
+        all_dists[i] = dist;
+        google_places_results[i].distance = dist;
+    }
+    let closest_results = [];
+    let taken_indices = [];
+    for (let k = 0; k < amt; k++) {
+        let lowest_dist = Infinity;
+        let low_dist_index = -1;
+        for (let i = 0; i < google_places_results.length; i++) {
+            if (all_dists[i] < lowest_dist && !taken_indices.includes(i)) {
+                lowest_dist = all_dists[i];
+                low_dist_index = i;
+            }
+        }
+        closest_results.push(google_places_results[low_dist_index]);
+        taken_indices.push(low_dist_index);
+    }
+    return closest_results;
+}
 main();
 //# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -24,8 +24,57 @@ const fetch_locations = (_: any, response: any) => {
       //     console.log(e.geometry.location);
       //   });
       console.log(`${res.data.results.length} results`);
-      response.send(JSON.stringify(res.data.results, null, 2));
+      
+      let closest_results = find_closest_results(location.lat, location.lng, res.data.results, 10);
+
+      response.send(JSON.stringify(closest_results, null, 2));
     });
 };
+
+function dist_between_coords(lat1: number, lng1:number, lat2:number, lng2:number) {
+  const R = 6371e3; // metres
+  const φ1 = lat1 * Math.PI/180; // φ, λ in radians
+  const φ2 = lat2 * Math.PI/180;
+  const Δφ = (lat2-lat1) * Math.PI/180;
+  const Δλ = (lng2-lng1) * Math.PI/180;
+
+  const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+          Math.cos(φ1) * Math.cos(φ2) *
+          Math.sin(Δλ/2) * Math.sin(Δλ/2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+  const d = R * c; // in metres
+  return d;
+}
+
+function find_closest_results(lat1: number, lng1: number, google_places_results: any, amt: number) {
+  let all_dists = [];
+
+  for (let i = 0; i < google_places_results.length; i++) {
+    const lat2 = google_places_results[i].geometry.location.lat;
+    const lng2 = google_places_results[i].geometry.location.lng;
+    const dist = dist_between_coords(lat1, lng1, lat2, lng2);
+    all_dists[i] = dist;
+    google_places_results[i].distance = dist;
+  }
+
+  let closest_results: number[] = []
+  let taken_indices: number[] = []
+
+  for (let k = 0; k < amt; k++) {
+    let lowest_dist = Infinity;
+    let low_dist_index = -1;
+    for (let i = 0; i < google_places_results.length; i++) {
+      if (all_dists[i] < lowest_dist && !taken_indices.includes(i)) {
+        lowest_dist = all_dists[i];
+        low_dist_index = i;
+      }
+    }
+    closest_results.push(google_places_results[low_dist_index]);
+    taken_indices.push(low_dist_index);
+  }
+
+  return closest_results;
+}
 
 main();
