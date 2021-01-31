34d221f2b8998ad53eae0d4ed59288beb459a569
Ranger Chenore <r2chenore@gmail.com>
2021-01-31 00:39:59 -0500

    Add weighted average functions, make compass work

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Compass from './Compass';
 
 function App() {
-
-  console.log("App started at least");
-
-  return (<div className="App">
-    <header className="App-header">
-      <div className="compass">
-      <div className="arrow"></div>
-      <div className="compass-circle"></div>
-      <div className="my-point"></div>
-    </div>
-      <button className="start-btn" onClick={() => start_compass()}>Start compass</button></header></div>);
-}
-
-function start_compass() {
-  console.log("in start_compass()");
-  navigator.geolocation.getCurrentPosition(locationHandler);
-  const compassCircle:any = document.querySelector(".compass-circle");
-  const startBtn = document.querySelector(".start-btn");
-  const myPoint = document.querySelector(".my-point");
-  let compass;
-
-  function handler(e: any) {
-    compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
-    compassCircle.style.transform = `translate(-50%, -50%) rotate(${-compass}deg)`;
-  }
 
   // Qibla geolocation
   const location = {
@@ -35,55 +11,14 @@ function start_compass() {
     lng: 39.826206,
   };
 
-  const isIOS = !(
-    navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
-    navigator.userAgent.match(/AppleWebKit/)
+  const [dest_lat, setLat] = useState(location.lat);
+  const [dest_lng, setLng] = useState(location.lng);
+
+  return (<div className="App">
+    <header className="App-header">
+      <Compass lat={dest_lat} lng={dest_lng} />
+    </header></div>
   );
-
-  let pointDegree;
-
-  function locationHandler(position: any) {
-    const { latitude, longitude } = position.coords;
-    pointDegree = calcDegreeToPoint(latitude, longitude);
-
-    if (pointDegree < 0) {
-      pointDegree = pointDegree + 360;
-    }
-  }
-
-  function calcDegreeToPoint(latitude: number, longitude: number) {
-
-    const phiK = (latitude * Math.PI) / 180.0;
-    const lambdaK = (longitude * Math.PI) / 180.0;
-    const phi = (latitude * Math.PI) / 180.0;
-    const lambda = (longitude * Math.PI) / 180.0;
-    const psi =
-      (180.0 / Math.PI) *
-      Math.atan2(
-        Math.sin(lambdaK - lambda),
-        Math.cos(phi) * Math.tan(phiK) -
-        Math.sin(phi) * Math.cos(lambdaK - lambda)
-      );
-    return Math.round(psi);
-  }
-
-  console.log("Before IOS check");
-  if (isIOS) {
-    console.log("Is definitely IOS");
-    DeviceOrientationEvent.requestPermission()
-      .then((response) => {
-        if (response === "granted") {
-          window.addEventListener("deviceorientation", handler, true);
-        } else {
-          alert("has to be allowed!");
-        }
-      })
-      .catch(() => alert("not supported"));
-  } else {
-    console.log("Not IOS");
-    window.addEventListener("deviceorientationabsolute", handler, true);
-  }
-
 }
 
 export default App;
--- server/src/util.ts
+++ server/src/util.ts
@@ -96,6 +96,64 @@ export const fetch_nearby_places = async (
   );
 };
 
+export const deg_to_rad = (degrees: number) => {
+  return degrees*2*Math.PI/360;
+}
+
+export const rad_to_deg = (degrees: number) => {
+  return degrees*360/2/Math.PI;
+}
+
+export const geo_coords_to_cartesian = (lat_deg: number, lng_deg: number) => {
+  const lat_rad = deg_to_rad(lat_deg);
+  const lng_rad = deg_to_rad(lng_deg);
+
+  const x = Math.sin(lat_rad) * Math.cos(lng_rad);
+  const y = Math.sin(lat_rad) * Math.sin(lng_rad);
+  const z = Math.cos(lat_rad); 
+
+  return {x, y, z};
+}
+
+export const cartesian_to_geo_coords = (x: number, y: number, z: number) => {
+  const lat = rad_to_deg(Math.atan2(z, Math.sqrt(x * x + y * y)))
+  const lng = rad_to_deg(Math.atan2(-y, x));
+  return {lat, lng};
+}
+
+export const weighted_avg_geo_coords = (geo_coords: any) => {
+  // geo_coords should be formatted: [{lat: 22.12313, lng: 41.3123, weight: 3}, {lat: 31.213, lng: 41.3123, weight: 8}, ...]
+
+  // sums all weights
+  let totalWeight = 0;
+  geo_coords.array.forEach((element:any) => {
+    totalWeight += element.weight;
+  });
+
+  // makes new coords object with cartesian instead of geo coords
+  let cartesian_coords: any = [];
+  geo_coords.array.forEach((element:any) => {
+    let {x, y, z} = geo_coords_to_cartesian(element.lat, element.lng);
+    cartesian_coords.push({x: x, y: y, z: z, weight: element.weight});
+  });
+
+  // gets the weighed average of all cartesian coords
+  let weighted_x_avg = 0;
+  let weighted_y_avg = 0;
+  let weighted_z_avg = 0;
+  cartesian_coords.forEach((element: any) => {
+    weighted_x_avg += element.x*element.weight;
+    weighted_y_avg += element.y*element.weight;
+    weighted_z_avg += element.z*element.weight;
+  });
+  weighted_x_avg /= totalWeight;
+  weighted_y_avg /= totalWeight;
+  weighted_z_avg /= totalWeight;
+
+  // converts back to geo coords and returns
+  return cartesian_to_geo_coords(weighted_x_avg, weighted_y_avg, weighted_z_avg);
+}
+
 export const get_filtered_pois = async (
   location: location,
   radius: number,
